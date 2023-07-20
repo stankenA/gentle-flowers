5ec26d6bf681719e2a76ab5f0ee92f285390bc8c
stanken_a <artyemst3@mail.ru>
2023-07-20 13:46:04 +0300

    feat: add promo swiper

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -27,3 +27,15 @@
 
 // const header = new Header('.header');
 // header.setEventListeners();
+
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js';
+
+const promoSwiper = new Swiper('.promo__swiper', {
+  slidesPreView: 1,
+  spaceBetween: 20,
+  grabCursor: true,
+  pagination: {
+    el: ".promo__swiper-pagination",
+    clickable: true,
+  },
+});
